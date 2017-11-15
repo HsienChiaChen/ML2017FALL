d9316523cc01e25f51222f73e7a83a9de1e9137e
HsienChiaChen <b03901149@ntu.edu.tw>
2017-11-16 00:30:04 +0800

    Upload hw3 Version 2.0

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-wget http://github.com/HsienChiaChen/ML2017FALL/releases/download/hw3_model/model-0.62792.h5
-wget http://github.com/HsienChiaChen/ML2017FALL/releases/download/hw3_model/model-0.62898.h5
-wget http://github.com/HsienChiaChen/ML2017FALL/releases/download/hw3_model/model-0.63715.h5
-wget http://github.com/HsienChiaChen/ML2017FALL/releases/download/hw3_model/model-0.63889.h5
 python3 mytest.py $1 $2 model-0.62792.h5 model-0.62898.h5 model-0.63715.h5 model-0.63889.h5 
