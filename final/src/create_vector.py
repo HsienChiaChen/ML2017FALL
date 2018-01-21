09e73ecce0b06fce99c1466e0fbfb874eba2ac49
HsienChiaChen <b03901149@ntu.edu.tw>
2018-01-21 11:41:42 +0800

    delete some code

--- final/src/create_vector.py
+++ final/src/create_vector.py
@@ -1,10 +1,9 @@
 import numpy as np
 import sys
-import os  #mode = 3
+import os
 from gensim.models import word2vec
 import jieba
 
-#str1 = "provideData/training_data/"
 str1 = sys.argv[1]
 str2 = "_train.txt"
 train_data = []
@@ -12,15 +11,12 @@ for i in range(5):
 	str4 = str(i+1)+str2
 	str3 = os.path.join(str1,str4)
 	train_data.append(str3)
-#jiebaDict = "dict.txt.big"
 jiebaDict = sys.argv[2]
 stop_file = sys.argv[3]
 
 print(">>>>> Loading Data ...")
-
-train = []
-
 print("   >>> Training set")
+train = []
 for i in range(len(train_data)):
 	with open(train_data[i],'r',encoding = 'UTF-8') as f:
 		l = f.readline()
@@ -34,9 +30,11 @@ for i in range(len(train_data)):
 jieba.set_dictionary(jiebaDict)
 stopwords = [line.rstrip() for line in open(stop_file,'r')]
 
+
+# Cut the sentence
 for i in range(len(train)):
 	train[i] = list(jieba.cut(train[i],cut_all = False))
-	# add stopword
+	# remove stopword
 	if True:
 		j = 0
 		while j != len(train[i]):
@@ -45,6 +43,7 @@ for i in range(len(train)):
 				j -= 1
 			j += 1
 
+# Output the corpus
 outputfile = open("corpus.txt",'w')
 sentence_unit = 1
 for i in range(len(train)):
@@ -60,6 +59,7 @@ for i in range(len(train)):
 outputfile.close()
 print("Corpus Output Successfully")
 
+# Training model
 sentences = word2vec.Text8Corpus("corpus.txt")
 model = word2vec.Word2Vec(sentences, size = 100,min_count=5,window = 7,sg = 1)
 model.save("word_vector.model.bin")
--- final/src/mytrain.py
+++ final/src/mytrain.py
@@ -9,15 +9,7 @@ from scipy import spatial
 # ================= #
 # File Path Setting #
 # ================= #
-'''str1 = "../provideData/training_data/"
-str2 = "_train.txt"
-train_data = []
-for i in range(5):
-	str3 = str1+str(i+1)+str2
-	train_data.append(str3)'''
-#test_data     = "../provideData/testing_data.csv" 
-#model_file = "m0116chinese.model.bin"
-#jiebaDict = "dict.txt.big"
+
 test_data = sys.argv[1]
 model_file = sys.argv[2]
 jiebaDict = sys.argv[3]
@@ -104,12 +96,8 @@ def calsimilarity(k):
 	# temp -> question string
 	temp = testq[k].split()
 	sim = []
-	#print()
-	#print(testq[k].replace(" ",""))
+
 	# For each answer, compute similarity
-	PAIR_AVG = True
-	PAIR_AVG_N = False
-	SENT_AVG = False
 	for choose in range(6):
 
 		# ansstr -> answer string
@@ -117,59 +105,21 @@ def calsimilarity(k):
 		oov = 0
 		sumsim = 0.0
 
-		if PAIR_AVG:
-			len_a = len(ansstr)
-			ansstr = [w for w in ansstr if w in model.wv.vocab]
-			temp = [w for w in temp if w in model.wv.vocab]
-			for i in range(len(temp)):
-				for j in range(len(ansstr)):
-					prob = model.similarity(temp[i],ansstr[j])
-					if prob > 0.2:
-						sumsim += prob
-			if len_a > 0:
-				sumsim /= float(len_a)
-			else:
-				sumsim = 0.0
-			sim.append(sumsim)
-		elif PAIR_AVG_N:
-			# get similarity from each pair of words (1 in Q, 1 in A)
-			len_a = len(ansstr)
-			ansstr = [w for w in ansstr if w in model.wv.vocab]
-			temp = [w for w in temp if w in model.wv.vocab]
-			ansstr = [w for w in ansstr if w not in temp]
-			try:
-				sumsim = model.n_similarity(temp,ansstr)
-			except:
-				sumsim = 0.0
-			sim.append(sumsim)
-			#print(testa[k][choose],sumsim)
-		elif SENT_AVG:
-			len_a = len(ansstr)
-			ansstr = [w for w in ansstr if w in model.wv.vocab]
-			temp = [w for w in temp if w in model.wv.vocab]
-			v_t = np.zeros((100,))
-			v_a = v_t
-			for w in temp:
-				v_t += model.wv[w]
-			for w in ansstr:
-				v_a += model.wv[w]
-			if len(temp) > 0:
-				v_t /= float(len(temp))
-			else:
-				v_t = np.zeros((100,))
-			if len(ansstr) > 0:
-				v_a /= float(len(ansstr))	
-			else:
-				v_a = np.zeros((100,))
-			if np.linalg.norm(v_t) != 0.0 and np.linalg.norm(v_a) != 0.0:
-				sumsim = 1-spatial.distance.cosine(v_t,v_a)
-			else:
-				sumsim = 0.0
-			sim.append(sumsim)
+		len_a = len(ansstr)
+		ansstr = [w for w in ansstr if w in model.wv.vocab]
+		temp = [w for w in temp if w in model.wv.vocab]
+		for i in range(len(temp)):
+			for j in range(len(ansstr)):
+				prob = model.similarity(temp[i],ansstr[j])
+				if prob > 0.2:
+					sumsim += prob
+		if len_a > 0:
+			sumsim /= float(len_a)
+		else:
+			sumsim = 0.0
+		sim.append(sumsim)
 
 	sim = np.array(sim)
-	#print(sim)
-	#print("=======> "+str(testa[k][np.argmax(sim)].replace(" ","")))
 	return(np.argmax(sim))
 
 def predict_answer():
@@ -192,27 +142,5 @@ def predict_answer():
 load_data()
 data_preprocessing()
 model = gensim.models.Word2Vec.load(model_file)
-
-'''for i in range(len(testq)):
-	l = testq[i].split()
-	for j in l:
-		if j not in model.wv.vocab:
-			testq[i] = testq[i].replace(j,'')
-			s = ""
-			for k in range(len(j)):
-				s += (j[k] + " ")
-			testq[i] = testq[i] + s
-
-for i in range(len(testa)):
-	for j in range(len(testa[i])):
-		l = testa[i][j].split()
-		for k in l:
-			if k not in model.wv.vocab:
-				testa[i][j] = testa[i][j].replace(k,'')
-				s = ""
-				for kk in range(len(k)):
-					s += (k[kk] + " ")
-				testa[i][j] = testa[i][j] + s'''
-
 predict_answer()
 
